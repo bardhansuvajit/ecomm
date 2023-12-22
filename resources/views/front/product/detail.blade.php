fbf5b329f4204d5e1f197928b8809db72cc6112a
Suvajit Bardhan <bardhansuvajit@gmail.com>
2023-12-23 00:45:00 +0530

    product detail category slug error fix

--- resources/views/front/product/detail.blade.php
+++ resources/views/front/product/detail.blade.php
@@ -71,13 +71,15 @@
                 <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="{{ route('front.home') }}">Home</a></li>
-                        {{ dd($categories) }}
                         @foreach ($categories as $category)
                             @if ($category->level == 1)
-                                <li class="breadcrumb-item"><a href="{{ route('front.category.detail.one', $category->c1_slug) }}">{{ $category->c1_title }}</a></li>
+                                @php
+                                    $parentSlug = $category->c1_slug;
+                                @endphp
+                                <li class="breadcrumb-item"><a href="{{ route('front.category.detail.one', $parentSlug) }}">{{ $category->c1_title }}</a></li>
                             @endif
                             @if ($category->level == 2)
-                                <li class="breadcrumb-item"><a href="{{ route('front.category.detail.two', [$categories[0]->c1_slug, $category->c2_slug]) }}">{{ $category->c2_title }}</a></li>
+                                <li class="breadcrumb-item"><a href="{{ route('front.category.detail.two', [$parentSlug, $category->c2_slug]) }}">{{ $category->c2_title }}</a></li>
                             @endif
                         @endforeach
                         <li class="breadcrumb-item active" aria-current="page">{{ $data->title }}</li>
